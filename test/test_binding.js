5a51a4aaa965754bfef8398faaf4e628aa1feb78
Yar Sawyer <idquantum@gmail.com>
2021-12-24 20:55:29 +0300

    tests

--- test/test_binding.js
+++ test/test_binding.js
@@ -7,8 +7,8 @@ function testBasic()
 {
     var data = new Buffer.from("0000002009f42768de3cfb4e58fc56368c1477f87f60e248d7130df3fb8acd7f6208b83a72f90dd3ad8fe06c7f70d73f256f1e07185dcc217a58b9517c699226ac0297d2ad60ba61b62a021d9b7700f0", "hex");
     const result =  yespower(data);
-    //console.log(result.toString("hex"));
-    assert(result, new Buffer.from("9d90c21b5a0bb9566d2999c5d703d7327ee3ac97c020d387aa2dfd0700000000","hex"), "Unexpected value returned");
+    console.log(result.toString("hex"));
+    assert.deepEqual(result, new Buffer.from("9d90c21b5a0bb9566d2999c5d703d7327ee3ac97c020d387aa2dfd0700000000","hex"), "Unexpected value returned");
 }
 
 assert.doesNotThrow(testBasic, undefined, "testBasic threw an expection");
